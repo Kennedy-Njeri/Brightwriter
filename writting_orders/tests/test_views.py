2d5ab4393dac3b99a7e627d87d7719adca291215
Kennedy Njeri <mistakenz123@gmail.com>
2019-05-29 23:40:14 +0300

    more tests

--- writting_orders/tests/test_views.py
+++ writting_orders/tests/test_views.py
@@ -8,58 +8,29 @@ from django.contrib.auth.models import User
 
 class OrderViewsTests(TestCase):
 
-    def setUp(self):
+    def test_order_list_GET(self):
 
 
-        self.user = User.objects.create(
-            username='author@test.com',
-            email='author@test.com',
-            password="30199278Ken"
-        )
 
-        self.type = Type.objects.create(
-            name="Theisis"
-        )
 
-        self.academic = AcademicLevel.objects.create(
+        self.list_url = reverse('order-list')
 
-            name="College",
+        response = self.client.get(self.list_url)
 
-        )
+        self.assertEquals(response.status_code, 302)
 
-        self.format = Format.objects.create(
 
-            name="CHICAGO"
-        )
 
-        self.order1 = Order.objects.create(
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('order-list'))
+        self.assertEquals(response.status_code, 200)
 
-            username=self.user,
-            type=self.type,
-            academic=self.academic,
-            topic="Networking",
-            pages=4,
-            urgency=3,
-            format=self.format,
-            instructions="Write in bold",
-            pdf="server.pdf",
-            total=8000,
-            paid=False,
-            email="kennedy@gmail.com",
 
-        )
 
-    def test_order_list_view_GET(self):
-        #self.list_url = reverse('order-list')
-        #response = self.client.get(self.list_url)
-        #self.assertIn(self.order, response)
-        #self.assertEquals(response.status_code, 200)
-        #self.assertTemplateUsed(response, 'order_list.html')
-        resp = self.client.get(reverse('order-list'))
-        self.assertEquals(resp.status_code, 200)
-        self.assertIn(self.order1)
-        self.assertTemplateUsed(resp, 'order_list.html')
-        print (resp.status_code)
+
+
+
+
 
 
 
