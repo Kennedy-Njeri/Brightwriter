5181bc7cda9aba3c14644fd0b775c6321a8affad
Kennedy Njeri <mistakenz123@gmail.com>
2019-05-31 22:04:11 +0300

    more tests

--- writting_orders/tests/test_views.py
+++ writting_orders/tests/test_views.py
@@ -8,22 +8,65 @@ from django.contrib.auth.models import User
 
 class OrderViewsTests(TestCase):
 
-    def test_order_list_GET(self):
+    def setUp(self):
+
+        self.client = Client()
+
+
+        self.user = User.objects.create(
+            username='author@test.com',
+            email='author@test.com',
+            password="30199278Ken"
+        )
+
+        self.type = Type.objects.create(
+            name="Theisis"
+        )
+
+        self.academic = AcademicLevel.objects.create(
+
+            name="College",
+
+        )
+
+        self.format = Format.objects.create(
+
+            name="CHICAGO"
+        )
+
+
+    def test_order_create_POST(self):
+
+        url = reverse('order-create')
+
+
+        response = self.client.post(url, {
+
+            'username': 'kennedy',
+            'type': 'self.type',
+            'academic': 'self.academic',
+            'topic': "Networking",
+            'pages': 4,
+            'urgency': 3,
+            'format': 'self.format',
+            'instructions': "Write in bold",
+            'pdf': "server.pdf",
+            'total': 8000,
+            'paid' : False,
+            'email': "kennedy@gmail.com",
+
+
+
+        })
+
+        order = Order.objects.get(id=3)
+
+        self.assertEquals(order.username, 'kennedy')
 
 
 
 
-        self.list_url = reverse('order-list')
 
-        response = self.client.get(self.list_url)
-
-        self.assertEquals(response.status_code, 302)
-
-
-
-    def test_view_url_by_name(self):
-        response = self.client.get(reverse('order-list'))
-        self.assertEquals(response.status_code, 200)
 
 
 
